07303aee71190f0c58b79945b1a93e7d3d606604
Visura Indula Malawara Arachchi <visuraindula2001@gmail.com>
2024-08-15 21:55:43 +0530

    database created

--- server-app/dist/server.js
+++ server-app/dist/server.js
@@ -5,11 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const dbConfig_1 = __importDefault(require("./config/dbConfig"));
 dotenv_1.default.config();
+(0, dbConfig_1.default)();
 const port = process.env.PORT || 5001;
 const app = (0, express_1.default)();
 app.get("/", (rq, res) => {
-    res.send("Hello");
+    res.send("Hello world");
 });
 app.listen(port, () => {
     console.log(`Server started successfully on port :${port}`);
--- server-app/src/server.ts
+++ server-app/src/server.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDb from "./config/dbConfig";
 dotenv.config();
+connectDb();
 
 const port = process.env.PORT || 5001;
 const app = express();
