38c5bdb8f89a9f855e7c5bb2bacf4d71bc34ef0b
Visura Indula Malawara Arachchi <visuraindula2001@gmail.com>
2024-08-17 02:05:28 +0530

    routes

--- server-app/src/server.ts
+++ server-app/src/server.ts
@@ -2,15 +2,23 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDb from "./config/dbConfig";
+import shortUrl from "./routes/shortUrl"
+import { json } from "stream/consumers";
 dotenv.config();
 connectDb();
 
 const port = process.env.PORT || 5001;
 const app = express();
 
-app.get("/",(rq,res) =>{
-    res.send("Hello world");
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended :true}))
+app.use(cors({
+    origin : "http://localhost:3000",
+    credentials:true,
+}))
+
+
+app.use("/api/", shortUrl);
 
 app.listen(port,() =>{
     console.log(`Server started successfully on port :${port}`);
