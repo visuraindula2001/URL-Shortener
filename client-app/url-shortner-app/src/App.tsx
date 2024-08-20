9ca57dc7d07545339f12f81f76c681e1cad5924e
Visura Indula Malawara Arachchi <visuraindula2001@gmail.com>
2024-08-21 02:01:31 +0530

    App.tsx

--- client-app/url-shortner-app/src/App.tsx
+++ client-app/url-shortner-app/src/App.tsx
@@ -1,10 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+import * as React from 'react';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+interface IAppProps {
 }
 
+const App: React.FunctionComponent<IAppProps> = () => {
+  return (
+    <>
+    <Header/>
+    <Footer/>
+    </>
+  );
+};
+
 export default App;
+
