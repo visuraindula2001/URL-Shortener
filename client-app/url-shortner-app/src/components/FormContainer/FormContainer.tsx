a707b132e346f485dddc4094e6b0a77f816aaba8
Visura Indula Malawara Arachchi <visuraindula2001@gmail.com>
2024-08-21 22:30:08 +0530

    input

--- client-app/url-shortner-app/src/components/FormContainer/FormContainer.tsx
+++ client-app/url-shortner-app/src/components/FormContainer/FormContainer.tsx
@@ -1,15 +1,38 @@
-import * as React from 'react';
+import * as React from "react";
 
+interface IFormContainerProps {}
 
-interface IFormContainerProps {
-}
-
-const FormContainer: React.FunctionComponent<IFormContainerProps> = (props) => {
+const FormContainer: React.FunctionComponent<IFormContainerProps> = () => {
   return (
-    <div className='container mx-auto p-2'>
-        <div className="bg-banner my-8 rounded-xl bg-cover">
-            <h2>URL Shortner</h2>
+    <div className="container mx-auto p-2">
+      <div className="bg-banner my-8 rounded-xl bg-cover bg-center">
+        <div className="w-full h-full rounded-xl p-20 backdrop-brightness-50">
+          <h2 className="text-white text-4xl text-center pb-4">URL Shortner</h2>
+          <p className="text-white text-center pb-2 text-xl font-extralight">
+            Please provide the link to be shortened
+          </p>
+          <p className="text-white text-center pb-4 text-sm font-thin">
+            Use our free URL shortener to create a shortened, clean link that's
+            easy to use
+          </p>
+          <form action="">
+            <div className="flex">
+              <div className="relative w-full">
+                <div className="absolute inset-y-0 start-0 flex items-center ps-2 pointer-events-none text-slate-800">
+                  urlshortner.link/
+                </div>
+                <input
+                  type="text"
+                  placeholder="add your link"
+                  required
+                  className="block w-full p-4 ps-32 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500"
+                />
+                
+              </div>
+            </div>
+          </form>
         </div>
+      </div>
     </div>
   );
 };
